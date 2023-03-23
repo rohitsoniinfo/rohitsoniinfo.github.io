c850fea08229401ecb0b85a691a3ed88387ecd43
rohit <rohitsoni181001@gmail.com>
2023-03-24 02:20:25 +0530

    add

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "61d1e30be2119c31b35d3c1412973e8b",
-"/": "61d1e30be2119c31b35d3c1412973e8b",
+"index.html": "8e18145dae5787749e69731761285174",
+"/": "8e18145dae5787749e69731761285174",
 "main.dart.js": "5b8580b3152537292ddd0c74174e7b4d",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
